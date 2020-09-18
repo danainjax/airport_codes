84def17e7654f41d96f7ad4da493fdf53e198c02
danainjax <danainjax@gmail.com>
2020-09-18 10:14:20 -0400

    added Scraper class

--- lib/airport_codes.rb
+++ lib/airport_codes.rb
@@ -2,6 +2,7 @@ require_relative "./airport_codes/version"
 require_relative "./airport_codes/cli"
 require_relative "./airport_codes/api"
 require_relative "./airport_codes/airport"
+require_relative "./airport_codes/scraper"
 
 module AirportCodes
   class Error < StandardError; end
